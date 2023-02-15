b0243cfeb02ac28197f6f946cf467dbd04a4ff0e
Cattimus <devcattimus@gmail.com>
2023-02-15 15:50:59 -0800

    Updated engine outline

--- src/Engine.hpp
+++ src/Engine.hpp
@@ -17,25 +17,39 @@ private:
 	int screenWidth = 640;
 	int screenHeight = 480;
 
+	//flags and configuration options
+	bool vsync = true;
+	int maxFps = 60;
+	
+	//parameters for cleanup passes
+	bool cleanupTextures = true;
+	int cleanupIntervalFrames = 300;
+
+	//delta value for logic
+	double delta = 0;
+
+	//SDL values
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
-	Scene* selectedScene = NULL;
 	uint background_color[3];
 
-	//list of textures
-	vector<unique_ptr<Texture>> textures;
+	//selected scene to be rendered
+	Scene* scene = NULL;
 
-	//list of scenes
+	//lists for data
+	vector<unique_ptr<Texture>> textures;
 	vector<unique_ptr<Scene>> scenes;
 
-	//user input callback
+	//user input callbacks
 	void (*OnKeyboardInput)() = NULL;
 	void (*OnMouseInput)() = NULL;
 	void (*OnControllerInput)() = NULL;
 
 	//helper functions for engine
 	bool Init();
-	void RenderBlank();
+	void Quit(); //NOTE - destructor must be called on textures before SDL_Quit() is called.
+	void MainLoop();
+	void TextureCleanup(); //Delete unreferenced textures
 
 public:
 	Engine();
@@ -48,7 +62,7 @@ public:
 	Scene* CreateScene(string name);
 	Scene* SwitchScene(string);
 	void   DeleteScene(string);
-	//mechanism to get the names of every scene
+	string GetSceneList();
 
 	//texture management functions
 	Texture* CreateTexture(string path);
@@ -60,8 +74,12 @@ public:
 	void RegisterMouseCallback(void (*)());
 	void RegisterControllerCallback(void (*)());
 
-	//main logic and rendering
-	void StartMainLoop();
-	void RenderScene();
+	//main loop and rendering
 	void SetBackgroundColor(uint, uint, uint);
+	void RenderCurrent();
+	void StartMainLoop();
+
+	//call logic functions
+	void LogicStep();
+	void LogicStep(double delta);
 };
