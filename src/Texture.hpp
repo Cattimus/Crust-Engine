3d03126fb44c73584d323fb8babbda4c9d4b72fd
Cattimus <devcattimus@gmail.com>
2023-02-15 01:28:20 -0800

    Texture outline added

---
+++ src/Texture.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include<iostream>
+using namespace std;
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+class Texture
+{
+private:
+	SDL_Texture* tex = NULL;
+	uint ref = 0;
+
+public:
+	Texture(string);
+	~Texture();
+
+	//reference counting functions
+	void RefInc();
+	void RefDec();
+	uint GetRef();
+
+	//get raw SDL texture
+	SDL_Texture* GetTex();
+};
