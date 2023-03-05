4001dbcca0e3e787deef0714cf7a40765caba521
Cattimus <devcattimus@gmail.com>
2023-03-04 17:30:23 -0800

    Prototype event class

---
+++ proto/Event.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+template <typename T>
+class Event
+{
+private:
+	string name;
+
+	//Pointer to parent object that the event will act upon
+	T* parent;
+
+	//Function pointer to condition
+	bool (*condition)(T* parent);
+
+	//Function pointer to action
+	void (*action)(T* parent);
+
+
+public:
+	Event(T* parent, string name, bool (*condition)(T*), void (*action)(T*))
+	{
+		this->parent = parent;
+		this->name = name;
+		this->condition = condition;
+		this->action = action;
+	}
+
+	Event(T* parent, string name)
+	{
+		this->parent = parent;
+		this->name = name;
+	}
+
+	//Register condition
+	void RegisterCondition(bool (*func)(T*))
+	{
+		condition = func;
+	}
+
+	//Register action
+	void RegisterAction(void (*func)(T*))
+	{
+		action = func;
+	}
+
+	void check()
+	{
+		//check for null condition or action
+		if(!(condition && action))
+		{
+			return;
+		}
+
+		//perform action if condition is met
+		if(condition(parent))
+		{
+			action(parent);
+		}
+	}
+};
