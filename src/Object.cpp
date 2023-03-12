ccd5313639188f39cc7a10e2f2a05039caca2cb5
Cattimus <devcattimus@gmail.com>
2023-03-12 15:31:26 -0700

    Object has been simplified to be only position data and events

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,76 +1,16 @@
 #include "Object.hpp"
 
-void Object::Init()
+Object::Object(uint id, int x, int y, int w, int h) : Position(x,y,w,h)
 {
-	id = 0;
-	tex = NULL;
-	SetPosition(0,0);
-	SetSize(0,0);
-	SetVelocity(0,0);
-	SetRotationOffset(0,0);
-	rotation = 0;
-	rotVeloc = 0;
-}
-
-Object::~Object()
-{
-	//Decrease reference count if texture exists
-	if(tex)
-	{
-		tex->RefDec();
-	}
-}
-
-Object::Object(uint id, Texture* tex, int x, int y, int w, int h)
-{
-	Init();
 	this->id = id;
-
-	this->tex = tex;
-	tex->RefInc();
-
-	SetPosition(x,y);
-	SetSize(w,h);
 }
-Object::Object(uint id, Texture* tex, int w, int h)
+Object::Object(uint id, int w, int h) : Position(x,y)
 {
-	Init();
 	this->id = id;
-
-	this->tex = tex;
-	tex->RefInc();
-
-	SetSize(w,h);
-}
-Object::Object(uint id, Texture* tex)
-{
-	Init();
-	this->id = id;
-
-	this->tex = tex;
-	tex->RefInc();
-
-	SetSize(tex->GetWidth(), tex->GetHeight());
 }
 
-Object::Object(uint id, int x, int y, int w, int h)
+Object::Object(uint id) : Position()
 {
-	Init();
-
-	this->id = id;
-	SetPosition(x,y);
-	SetSize(w,h);
-}
-Object::Object(uint id, int w, int h)
-{
-	Init();
-
-	this->id = id;
-	SetSize(w,h);
-}
-Object::Object(uint id)
-{
-	Init();
 	this->id = id;
 }
 
@@ -79,91 +19,12 @@ uint Object::GetID()
 	return id;
 }
 
-void Object::SetPosition(double x, double y)
-{
-	pos[0] = x;
-	pos[1] = y;
-}
-double Object::GetXPosition()
-{
-	return pos[0];
-}
-double Object::GetYPosition()
-{
-	return pos[1];
-}
-
-void Object::SetVelocity(double x, double y)
-{
-	vel[0] = x;
-	vel[1] = y;
-}
-double Object::GetXVelocity()
-{
-	return vel[0];
-}
-double Object::GetYVelocity()
-{
-	return vel[1];
-}
-
-void Object::SetSize(int w, int h)
-{
-	size[0] = w;
-	size[1] = h;
-}
-int Object::GetWidth()
-{
-	return size[0];
-}
-int Object::GetHeight()
-{
-	return size[1];
-}
-
-SDL_Texture* Object::GetTexture()
-{
-	return tex->Get();
-}
-Texture* Object::GetTextureObject()
-{
-	return tex;
-}
-
-void Object::SetRotation(double rotation)
-{
-	this->rotation = rotation;
-}
-void Object::SetRotationVelocity(double rotVeloc)
-{
-	this->rotVeloc = rotVeloc;
-}
-double Object::GetRotation()
-{
-	return rotation;
-}
-double Object::GetRotationVelocity()
-{
-	return rotVeloc;
-}
-
-void Object::SetRotationOffset(double x, double y)
-{
-	centerOffset[0] = x;
-	centerOffset[1] = y;
-}
-
-double Object::GetRotationOffsetX()
-{
-	return centerOffset[0];
-}
-
-double Object::GetRotationOffsetY()
-{
-	return centerOffset[1];
-}
-
 EventHandler<Object>* Object::GetEventHandler()
 {
 	return &events;
+}
+
+bool Object::IsCollidingWith(Object& b)
+{
+	
 }
--- src/Object.hpp
+++ src/Object.hpp
@@ -4,104 +4,31 @@
 #include <unordered_map>
 using namespace std;
 
-#include "Texture.hpp"
 #include "EventHandler.hpp"
+#include "Position.hpp"
 
-class Object
+class Object : public Position
 {
 private:
 
 	//Unique ID for the object (at least in the scene)
 	uint id;
 
-	//Pointer to the texture object that will be used for rendering
-	Texture* tex;
-
-	//Position data (x,y)
-	double pos[2];
-
-	//Center of rotation
-	double centerOffset[2];
-
-	//Velocity data (x,y)
-	double vel[2];
-
-	//Size data (w,h)
-	int    size[2];
-
-	double rotation;
-	double rotVeloc;
-
 	//Collection of all events sorted by name
 	EventHandler<Object> events;
 
-	//Helper functions to initialize values
-	void Init();
-
 public:
-	//Should this object be rendered in front of(+) or behind(-) other objects
-	int renderPriority;
-	//Should this object be rendered
-	bool visible;
-
-//////////////CONSTRUCTORS - VISIBLE OBJECT////////////////////////////
-
-	Object(uint id, Texture* tex, int x, int y, int w, int h);
-	Object(uint id, Texture* tex, int w, int h);
-	Object(uint id, Texture* tex);
-
-/////////////CONSTRUCTORS - POSITION ONLY/////////////////////////
-
 	Object(uint id, int x, int y, int w, int h);
 	Object(uint id, int w, int h);
 	Object(uint id);
 
-	//Decrease refcount on texture to make sure it is deleted
-	~Object();
-
 	//Return the object ID
 	uint GetID();
 
-////////////////POSITION FUNCTIONS//////////////////////////////
-
-	void SetPosition(double x, double y);
-	double  GetXPosition();
-	double  GetYPosition();
-
-//////////////////////VELOCITY FUNCTIONS//////////////////////////
-
-	void   SetVelocity(double x, double y);
-	double GetXVelocity();
-	double GetYVelocity();
-
-/////////////////////SIZE FUNCTIONS////////////////////////////
-
-	void SetSize(int w, int h);
-	int  GetWidth();
-	int  GetHeight();
-
-//////////////////////ROTATION////////////////////////////////
-
-	void SetRotation(double rotation);
-	double GetRotation();
-
-	void SetRotationVelocity(double rotVeloc);
-	double GetRotationVelocity();
-
-	void SetRotationOffset(double x, double y);
-	double GetRotationOffsetX();
-	double GetRotationOffsetY();
-
-///////////////////TEXTURE FUNCTIONS//////////////////////////
-
-	//Return the SDL_Texture* for rendering
-	SDL_Texture* GetTexture();
-
-	//Return the Texture object
-	Texture* GetTextureObject();
-
-//////////////////EVENT FUNCTIONS///////////////////////////
-
 	EventHandler<Object>* GetEventHandler();
+
+//////////////////////COLLISIONS///////////////////////////
+
+	bool IsCollidingWith(Object& b);
 
 };
