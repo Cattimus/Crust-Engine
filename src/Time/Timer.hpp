732bc570564e50aea010228d48f31d358ae82f63
Cattimus <github_commit@cattimus.dev>
2023-08-08 23:13:52 -0700

    Updated timer outline

--- src/Time/Timer.hpp
+++ src/Time/Timer.hpp
@@ -10,14 +10,23 @@ class Timer
 private:
 	int32_t startTime;
 	int32_t endTime;
+	bool elapsed;
+
+	//callback function for when the timer elapses
+	void (*onElapse)(void* args);
+	void* args;
 
 public:
 	bool autoReset;
 
 	bool Elapsed();
-	void Set(int32_t ms);
+	void Start(int32_t ms);
 	void Reset();
-	void OnElapse();
+	void Stop();
+	
 	int32_t TimeSinceStarted();
 	int32_t TimeRemaining();
+
+	void OnElapse(void (*callback)(void* args), void* args);
+	void CheckElapse();
 };
