7edaf0f7e04d80f279e0a68bb7017805ac60ce88
Cattimus <github_commit@cattimus.dev>
2023-08-12 11:09:28 -0700

    Timer outline updated

---
+++ src/Time/Timer.cpp
@@ -0,0 +1,84 @@
+#include "Timer.hpp"
+#include <SDL2/SDL_timer.h>
+
+//Initialize values
+void Timer::Init()
+{
+  startTime = 0;
+  endTime = 0;
+  enabled = false;
+  onElapse = nullptr;
+  args = nullptr;
+}
+
+bool Timer::Elapsed()
+{
+  if((SDL_GetTicks() - endTime) < 0)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+void Timer::Start(int32_t ms)
+{
+  startTime = SDL_GetTicks();
+  endTime = startTime + ms;
+}
+
+void Timer::Reset()
+{
+  //don't enable the timer if there's no callback set
+  if(!onElapse)
+  {
+    return;
+  }
+
+  //get the timer duration
+  int duration = endTime - startTime;
+  
+  //reset timer duration
+  startTime = endTime;
+  endTime += duration;
+
+  enabled = true;
+}
+
+void Timer::Stop()
+{
+  enabled = false;
+}
+
+int32_t Timer::TimeSinceStarted()
+{
+  return SDL_GetTicks() - startTime;
+}
+
+int32_t Timer::TimeRemaining()
+{
+  return endTime - SDL_GetTicks();
+}
+
+void Timer::OnElapse(void (*callback)(void* args), void* args)
+{
+  if(callback)
+  {
+    onElapse = callback;
+  }
+
+  if(args)
+  {
+    this->args = args;
+  }
+}
+
+//This needs to be called from the main loop to check
+void Timer::CheckElapse()
+{
+  //call callback if timer is elapsed
+  if(enabled && Elapsed())
+  {
+    onElapse(args);
+  }
+}
