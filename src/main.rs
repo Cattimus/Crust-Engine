2d1c12e222e09f76b1c8a0abc4d45d3d8bd4cc63
Cattimus <github_commit@cattimus.dev>
2024-02-23 16:48:40 -0800

    Breaking up project structure in a way that makes more sense

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod sdl_bindings;
+
 mod sdl;
 use sdl::*;
 
---
+++ src/sdl/mod.rs
@@ -0,0 +1,41 @@
+#![allow(dead_code)]
+
+pub mod window;
+pub use window::*;
+
+use crate::sdl_bindings::*;
+
+use std::ffi::*;
+
+pub fn crust_init() -> bool {
+	unsafe {
+		//init SDL
+		if SDL_Init(SDL_INIT_VIDEO) < 0 {
+			eprintln!("Unable to initialize SDL");
+			return false;
+		}
+
+		//set render scale to nearest neighbor
+		let value = CString::new("0").unwrap();
+		let hint = SDL_HINT_RENDER_SCALE_QUALITY.as_ptr() as *const c_char;
+		if SDL_SetHint(hint, value.as_ptr()) == 0 {
+			eprintln!("Unable to set billinear rendering");
+		}
+
+		//set vsync to on
+		let value = CString::new("1").unwrap();
+		let hint = SDL_HINT_RENDER_VSYNC.as_ptr() as *const c_char;
+		if SDL_SetHint(hint, value.as_ptr()) == 0 {
+			eprintln!("Unable to set vsync");
+		}
+
+		return true;
+	}
+
+}
+
+pub fn crust_exit() {
+	unsafe {
+		SDL_Quit();
+	}
+}
